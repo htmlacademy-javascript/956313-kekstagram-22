ab3f5748ec0758609c2a89703ddc93388306330e
AOhapkin <alexeyyago@gmail.com>
2021-02-01 23:21:29 +0300

    исправил ошибки в getRandomFromRange(), переписал checkCommentLength() с помощью тернарного оператора, добавил проверку передаваемых параметров

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@
 function getRandomFromRange (min, max) {
   let result = null;
   if (Number.isInteger(min) && Number.isInteger(max)) {
-    if (min > 0 && max > 0 && min < max) {
+    if (min >= 0 && max >= 0 && min <= max) {
       result = Math.floor(Math.random() * (max - min + 1) + min);
     }
   }
@@ -14,15 +14,13 @@ function getRandomFromRange (min, max) {
 // Проверка максимальной длины комментария
 
 function checkCommentLength (comment, maxCommentLength) {
-  let isCommentFits = false;
-
-  if (comment.length <= maxCommentLength) {
-    isCommentFits = true;
+  let isCommentFits = null;
+  if (typeof comment === 'string' && typeof maxCommentLength === 'number') {
+    isCommentFits = (comment.length <= maxCommentLength) ? true : false;
   }
-
   return isCommentFits;
 }
 
-getRandomFromRange(0, 9);
+getRandomFromRange(0, 0);
 
-checkCommentLength('sjdfeiugksufhksdufhsdkuhfskfhjk', 200);
+checkCommentLength('fd', 200);
