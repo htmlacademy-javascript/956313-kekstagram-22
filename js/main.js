9a201105e8428898bcc7ab2489bed8be8210e84b
AOhapkin <alexeyyago@gmail.com>
2021-02-04 21:41:51 +0300

    Генерация массива из 25 объектов с описаниями фотографий пользователей (без id комментария)

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
-// Случайное целое число из переданного диапазона включительно
-// Диапазон может быть только положительный, включая ноль
+'use strict'
 
 function getRandomFromRange (min, max) {
   let result = null;
@@ -11,8 +10,6 @@ function getRandomFromRange (min, max) {
   return result;
 }
 
-// Проверка максимальной длины комментария
-
 function checkCommentLength (comment, maxCommentLength) {
   let isCommentFits = null;
   if (typeof comment === 'string' && typeof maxCommentLength === 'number') {
@@ -21,6 +18,64 @@ function checkCommentLength (comment, maxCommentLength) {
   return isCommentFits;
 }
 
-getRandomFromRange(0, 0);
+const NAMES = [
+  'Иван',
+  'Себастьян',
+  'Мария',
+  'Кристоф',
+  'Наташа',
+  'Юлия',
+  'Кирилл',
+  'Саша',
+];
 
-checkCommentLength('fd', 200);
+const MESSAGES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+
+const DESCRIPTIONS = [
+  'Моё лучшее фото за всё время!',
+  'Сморите! Получилась красота)',
+  'Сейчас лучшее время, чтобы выложить фотографию.',
+  'Зачем стараться, если никто не поставит лайк этому фото?',
+  'Не могу придумать хорошее описание для этой фотографии. Есть идеи?',
+  'Я у мамы фотограф!',
+];
+
+const PHOTOS_COUNT = 25;
+
+function getRandomArrayElement (array) {
+  return array[getRandomFromRange(0, array.length - 1)];
+}
+
+function generatePhoto () {
+  return {
+    id: '',
+    url: '',
+    description: getRandomArrayElement(DESCRIPTIONS),
+    likes: getRandomFromRange(15, 200),
+    comments: {
+      id: '',
+      avatar: 'img/avatar-' + getRandomFromRange(1, 6) + '.svg',
+      message: getRandomArrayElement(MESSAGES),
+      name: getRandomArrayElement(NAMES),
+    },
+  }
+}
+
+function generatePhotosArray () {
+  let photos = new Array(PHOTOS_COUNT).fill(null).map(() => generatePhoto());
+  for (let i = 0; i < photos.length; i++) {
+    photos[i].id = i + 1;
+    photos[i].url = 'photos/' + (i + 1) + '.jpg';
+  }
+  //id комментария
+  return photos;
+}
+
+console.log(generatePhotosArray());
