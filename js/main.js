1c9e2a6e993219f5d6fa9ac512efcab0deb2b83c
AOhapkin <alexeyyago@gmail.com>
2021-02-15 18:16:32 +0300

    Отображение первой (только первой) фото в полноразмерном режиме

--- js/main.js
+++ js/main.js
@@ -2,5 +2,7 @@
 
 import {createPictures} from './create-content.js';
 import {generatePhotosArray} from './generate-data.js';
+import {showBigPicture} from './show-big-picture.js';
 
 createPictures(generatePhotosArray());
+showBigPicture();
