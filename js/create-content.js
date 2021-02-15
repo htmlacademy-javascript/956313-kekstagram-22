610aea2ae112f8f53d685af0ec0a211d274fd4f9
AOhapkin <alexeyyago@gmail.com>
2021-02-15 13:58:22 +0300

    Добавление новых элементов с помощью DocumentFragment

--- js/create-content.js
+++ js/create-content.js
@@ -1,17 +1,22 @@
-function createPictures (picturesData) {
-  const picturesSection = document.querySelector('.pictures');
-  const pictureTemplate =document.querySelector('#picture')
-    .content
-    .querySelector('.picture');
-  const userPictures = picturesData;
+const picturesSection = document.querySelector('.pictures');
+const pictureTemplate =document.querySelector('#picture')
+  .content
+  .querySelector('.picture');
 
-  userPictures.forEach((picture) => {
-    const pictureElement = pictureTemplate.cloneNode(true);
-    pictureElement.querySelector('.picture__img').src = picture.url;
-    pictureElement.querySelector('.picture__comments').textContent = picture.comments.length;
-    pictureElement.querySelector('.picture__likes').textContent = picture.likes;
-    picturesSection.appendChild(pictureElement);
+function createPictureElement (elementData) {
+  const pictureElement = pictureTemplate.cloneNode(true);
+  pictureElement.querySelector('.picture__img').src = elementData.url;
+  pictureElement.querySelector('.picture__comments').textContent = elementData.comments.length;
+  pictureElement.querySelector('.picture__likes').textContent = elementData.likes;
+  return pictureElement;
+}
+
+function createPictures (picturesData) {
+  const picturesSectionFragment = document.createDocumentFragment();
+  picturesData.forEach((picture) => {
+    picturesSectionFragment.appendChild(createPictureElement(picture));
   });
+  picturesSection.appendChild(picturesSectionFragment);
 }
 
 export {createPictures};
